b6cad80bd487d609d4fc6bffcf3aa8fca1b877f5
Dior.Shelton <dior.shelton@gmail.com>
2025-08-08 14:15:12 -0400

    move product funtions and structs from main.go to product.go

--- product.go
+++ product.go
@@ -3,3 +3,38 @@ package main
 type Product struct {
 	ID int
 }
+
+type Item struct {
+	ProductID int
+	Quantity  int
+}
+
+type User struct {
+	ID        int
+	Inventory []Item
+}
+
+func AddToInventory(user *User, productID, quantity int) {
+	for i, item := range user.Inventory {
+		if item.ProductID == productID {
+			user.Inventory[i].Quantity += quantity
+			return
+		}
+	}
+	user.Inventory = append(user.Inventory, Item{
+		ProductID: productID,
+		Quantity:  quantity,
+	})
+}
+
+func RemoveFromInventory(user *User, productID, quantity int) {
+	for i, item := range user.Inventory {
+		itemCount := user.Inventory[i].Quantity
+		//remove item if quantity <= 0
+		if item.ProductID == productID && itemCount-quantity <= 0 {
+			user.Inventory = append(user.Inventory[:i], user.Inventory[i+1:]...)
+			return
+		}
+		user.Inventory[i].Quantity -= quantity
+	}
+}
