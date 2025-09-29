1ed3046698e3d7fbb0d0002b39ad2f6fc73cbd58
Dior.Shelton <dior.shelton@gmail.com>
2025-09-29 17:38:15 -0400

    format main.go

--- db/db.go
+++ db/db.go
@@ -1,1 +1,55 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+// SetupTestUserDB creates a temporary in memory test user database
+func SetupTestUserDB() *sql.DB {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		fmt.Printf("Database Error:%v", err)
+	}
+
+	query := `
+		CREATE TABLE users (
+		id INTEGER PRIMARY KEY,
+		username TEXT NOT NULL UNIQUE,
+		first_name TEXT NOT NULL,
+		last_name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		password_hash TEXT NOT NULL,
+		balance INTEGER NOT NULL DEFAULT 0,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		fmt.Printf("Failed to create table: %v", err)
+	}
+	return  db
+}
+
+func SetupRefreshTokenDB() *sql.DB {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		fmt.Printf("Database Error:%v", err)
+	}
+
+	query := `
+		CREATE TABLE refresh_tokens (
+		id INTEGER PRIMARY KEY,
+		user_id TEXT NOT NULL UNIQUE,
+		token TEXT NOT NULL,
+		expires_at DATETIME NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		revoked BOOLEAN DEFAULT 0
+		)
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		fmt.Printf("Failed to create table: %v", err)
+	}
+	return  db
+}
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/diorshelton/golden-market/handlers"
+	"github.com/diorshelton/golden-market/auth"
+	"github.com/diorshelton/golden-market/db"
+	"github.com/diorshelton/golden-market/models"
 )
 
 func main() {
@@ -12,16 +17,23 @@ func main() {
 		Addr:    ":3000",
 		Handler: marketServer(),
 	}
-
 	log.Printf("Running sever on port%v", s.Addr)
 	log.Fatal(s.ListenAndServe())
 }
+
+var userDatabase = db.SetupTestUserDB()
+var refreshTokenDb = db.SetupRefreshTokenDB()
+
+var userRepo = models.NewUserRepository(userDatabase)
+var tokenRepo = models.NewRefreshTokenRepository(refreshTokenDb)
+
+// var service = auth.NewAuthService()
 
 func marketServer() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /sign-in", handleSignIn)
 	mux.HandleFunc("GET /register", handleRegister)
-	mux.HandleFunc("POST /register", handlers.HandleRegisterForm)
+	// mux.HandleFunc("POST /register", handlers.HandleRegisterForm)
 	return mux
 }
 
