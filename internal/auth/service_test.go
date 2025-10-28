e2683827e24ef42a318fae2555cec0abdba2b101
Dior.Shelton <dior.shelton@gmail.com>
2025-10-28 14:41:24 -0400

    remove unused imports

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -5,9 +5,7 @@ import (
 	"time"
 
 	"github.com/diorshelton/golden-market/internal/database"
-	// "github.com/diorshelton/golden-market/internal/models"
 	"github.com/diorshelton/golden-market/internal/repository"
-	// "github.com/google/uuid"
 )
 
 func setupTestService(t *testing.T) (*AuthService, func()) {
